6351075c3d09c6edd6bcaf859b5ed517ba45f376
grey275 <pjvanderpol@gmail.com>
2019-06-11 13:36:20 -0700

    complete exercise 6

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {minimum: -1}
+  validate :serve_at_least_one_gender?
 
+  def serve_at_least_one_gender?
+    unless mens_apparel || womens_apparel
+      errors.add(:mens_apparel, "must serve at least one gender")
+      errors.add(:womens_apparel, "must serve at least one gender")
+    end
+  end
 end
