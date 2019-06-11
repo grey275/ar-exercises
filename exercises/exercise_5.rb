3b78074b3d55d93f76b18011ea8ab803fc5574ff
grey275 <pjvanderpol@gmail.com>
2019-06-11 12:05:32 -0700

    completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+@count = Store.count
+
+puts "total revenue: " + @total_revenue.to_s
+puts "average revenue: " + (@total_revenue / @count).to_s
+
+@failing_count = Store
+  .where(annual_revenue: 0...1000000)
+  .count
+
+puts "failing_count: " + @failing_count.to_s
